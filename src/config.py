3509064368fd05879d1f75eae4816759e2691152
Ubuntu <emma.nojd@hotmail.com>
2026-03-17 19:15:14 +0000

    Updated worker 3 ip adress

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pyspark.sql.types import StructType, StructField, StringType
 from stop_words import get_stop_words
 
-HDFS_DATA_PATH = "hdfs://192.168.2.25:9000/user/ubuntu/data/reddit_sample.json"
+HDFS_DATA_PATH = "hdfs://192.168.2.25:9000/user/ubuntu/data/reddit_data.json"
 
 STOPWORDS = get_stop_words("en")
 
